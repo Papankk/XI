1087eba2155f42012e0045529cb988d45cf0bb32
Papankk <115204004+Papankk@users.noreply.github.com>
2023-08-20 13:56:11 +0700

    delete.php

--- delete.php
+++ delete.php
@@ -1,11 +1,9 @@
 <?php
-// Include database connection file
 include_once("koneksi.php");
 
 if(isset($_GET['nis'])) {
     $nis = $_GET['nis'];
     
-    // Delete user data from table
     $result = mysqli_query($mysqli, "DELETE FROM tb_siswa WHERE nis='$nis'");
 
     if ($result) {
